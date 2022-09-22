de158d6891c6f3483ac562ea6357370b5244ae24
Baskanbetul <btlbskn@hotmail.com>
2022-09-22 17:04:09 -0600

    Fix the bug in error handling

--- src/Components/ArticlesCard/ArticlesCard.js
+++ src/Components/ArticlesCard/ArticlesCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ArticlesCard.css';
 
-const ArticlesCard = ( {title, byline, multimedia, url, selectArticle} ) => {
+const ArticlesCard = ( {title, byline, multimedia, selectArticle} ) => {
   return (
     <div className='articles-card'>
       <p className='title'>{title}</p>
-      <Link to ='details'>
+      <Link to ='/article/details'>
         {multimedia ? <img src={multimedia[1].url} alt={multimedia[2].caption} onClick={(event) => selectArticle(event)}></img> : <></>}
       </Link> 
       <p className='byline'>{byline}</p> 
