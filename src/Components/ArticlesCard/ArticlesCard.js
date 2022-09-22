e335a4fdc8948b98bc95150725ebceddb31157fd
Baskanbetul <btlbskn@hotmail.com>
2022-09-22 14:55:33 -0600

    Fixed bugs and displayed detail page

--- src/Components/ArticlesCard/ArticlesCard.js
+++ src/Components/ArticlesCard/ArticlesCard.js
@@ -6,10 +6,8 @@ const ArticlesCard = ( {title, byline, multimedia, url, selectArticle} ) => {
   return (
     <div className='articles-card'>
       <p className='title'>{title}</p>
-      <Link to ='/'>
-        <img className='article-img' 
-        src={multimedia[0].url} alt={multimedia[1].caption} 
-        onClick={(event) => selectArticle(event)}></img>
+      <Link to ='details'>
+        {multimedia ? <img src={multimedia[1].url} alt={multimedia[2].caption} onClick={(event) => selectArticle(event)}></img> : <></>}
       </Link> 
       <p className='byline'>{byline}</p> 
     </div>
