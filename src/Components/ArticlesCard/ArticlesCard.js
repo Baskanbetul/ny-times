c27ed4e6ce0aceb9de638ce77ca3a45a04f8cb78
Baskanbetul <btlbskn@hotmail.com>
2022-09-22 19:32:31 -0600

    Fix articles card component functionality

--- src/Components/ArticlesCard/ArticlesCard.js
+++ src/Components/ArticlesCard/ArticlesCard.js
@@ -4,19 +4,14 @@ import './ArticlesCard.css';
 
 const ArticlesCard = ( {title, byline, multimedia, selectArticle} ) => {
   return (
-
-      <div className='articles-card'>
-        <b className='title'>{title}</b>
-        <Link to ='/'>
-          <img className='article-img' 
-          src={multimedia[0].url} alt={multimedia[1].caption} 
-          onClick={(event) => selectArticle(event)}></img>
-        </Link> 
-        <b className='byline'>{byline}</b> 
-      </div>
-
+    <div className='articles-card'>
+      <p className='title'>{title}</p>
+      <Link to ='/article/details'>
+        {multimedia ? <img src={multimedia[1].url} alt={multimedia[2].caption} onClick={(event) => selectArticle(event)}></img> : <></>}
+      </Link> 
+      <p className='byline'>{byline}</p> 
+    </div>
   )
 }
 
 export default ArticlesCard;
-
